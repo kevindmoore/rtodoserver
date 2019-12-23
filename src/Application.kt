dd9eee11740128d4a8120fed45fa952957d41f0e
Kevin Moore <kevindavmoore@gmail.com>
2019-12-23 09:57:46 -0800

    Add copyrights, update build

--- src/Application.kt
+++ src/Application.kt
@@ -31,23 +31,24 @@ package com.raywenderlich
 
 import JwtService
 import hash
-import io.ktor.application.*
-import io.ktor.response.*
-import io.ktor.request.*
-import io.ktor.routing.*
-import io.ktor.http.*
-import io.ktor.locations.*
-import io.ktor.sessions.*
-import io.ktor.auth.*
+import io.ktor.application.Application
+import io.ktor.application.install
+import io.ktor.auth.Authentication
 import io.ktor.auth.jwt.jwt
-import io.ktor.gson.*
-import io.ktor.features.*
+import io.ktor.features.ContentNegotiation
+import io.ktor.gson.gson
+import io.ktor.locations.KtorExperimentalLocationsAPI
+import io.ktor.locations.Locations
+import io.ktor.routing.routing
+import io.ktor.sessions.Sessions
+import io.ktor.sessions.cookie
 import io.ktor.util.KtorExperimentalAPI
 import models.MySession
 import repository.DatabaseFactory
 import repository.TodoRepository
 import routes.todos
 import routes.users
+import kotlin.collections.set
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
